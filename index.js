f4a1c0531a3a3d555b1ab7f34d90581ba2bc7422
Camilo Pinilla <cristian.camilo6@gmail.com>
2020-09-08 06:43:15 -0500

    Cleaning

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const { check, validationResult } = require('express-validator');
 var requestIp = require('request-ip');
 app.use(requestIp.mw())
 
-app.use(bodyParser.urlencoded({extended: false})); app.use(express.static((__dirname, 'public')));
+app.use(bodyParser.json());
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
@@ -72,148 +72,13 @@ const Product = sequelize.define('product', {
 Customer.sync()
 Product.sync()
 
-app.post('/suscribe-product-price', [
-    check('phone').isLength({min: 10, max: 10})
-], function (req, res) {
-    
-    const errors = validationResult(req)
-    
-    // TODO: Aquí se puede hacer una validación más precisa
-    if (!errors.isEmpty()) {
-        // console.log(res.status(422).json({ errors: errors.array() }))    
-        return res.status(422).send({success: false, message: `Error con los campos diligenciados`});
-    }
-    
-    var findCustomer = async () => Customer.findOne({ where: { phone: req.body.phone } });
+app.post('/payment-method/payu', function (req, res) {
 
-    findCustomer().then(resp => {
+    return res.status(200).send('jR46zZRser');
 
-        if(resp === null) {
+    console.log(req.body)
 
-            try {
-                var createCustomer = async () => Customer.create({
-                    name: req.body.name,
-                    phone: req.body.phone,
-                    productId: req.body.productid,
-                    ip: req.clientIp,
-                    rol: req.body.rol
-                });
-                
-                createCustomer().then(resp => {
-                    console.log(`User added.`);
-
-                    (async () => {
-                        try {
-                            var findProduct = await Product.findOne({ where: { id: req.body.productid } });
-                            
-                            osmosis
-                            .get(`https://www.corabastos.com.co/sitio/historicoApp2/reportes/historicos.php?c=${findProduct.corabastosCode}&d=ok`)
-                            .find('tbody')
-                            .set({'product': ['tr']})
-                            .data(function(listing) {
-                                
-                                var requestSms = {
-                                    'account': 10019189,
-                                    'apiKey': 'c1vdJfdQnzfe9rTyLkfmRaJRkMjmmN',
-                                    'token': 'b6d67aded0fbccf4888ccf0385ed5aac',
-                                    'toNumber': req.body.phone,
-                                    'sms': null
-                                }
-
-                                let htmlPrice = listing.product[0];
-
-                                var splitToPrice = htmlPrice.split("$");
-
-                                console.log(findProduct)
-                                console.log(splitToPrice)
-                                
-                                if(findProduct.unitAlternative === null || findProduct.factor < 1 || findProduct.published == '0') {
-                                    requestSms.sms = `Ziembra.co ${findProduct.name.toUpperCase()} Promedio Precio Venta Mayorista Bogotá Calidad Corriente Kilo ${splitToPrice[3].trim().replace('\t', '')}. Aplican T&C.`;
-                                }
-                                
-                                else {
-                                    var majorPrice = findProduct.factor * parseInt(splitToPrice[3].trim().replace(',', ''));
-                                    
-                                    var strMajorPrice = majorPrice.toString()
-
-                                    var finalMajorPrice = strMajorPrice.substr(0, strMajorPrice.length - 2) + '00';
-
-                                    // var roundMajorPrice = Math.round((majorPrice / 100))
-
-                                    // var finalMajorPrice = roundMajorPrice * 100
-                                    
-                                    requestSms.sms = `Ziembra.co ${findProduct.name.toUpperCase()} Promedio Precio Venta Mayorista Bogotá Calidad Corriente Kilo ${splitToPrice[3].trim().replace('\t', '')} ${findProduct.unitAlternative} $${finalMajorPrice}. Aplican T&C.`;
-                                }
-
-                                var date = new Date()
-
-                                console.log('Hora', date.getHours())
-
-                                // TODO: Definir UTC(-5)
-                                if(!(date.getHours() >= 5 & date.getHours() < 15)) {
-                                    try {
-                                        superagent
-                                            .post('https://api101.hablame.co/api/sms/v2.1/send/')
-                                            .type('form')
-                                            .send(requestSms)
-                                            .end((error, response) => {
-                                                if(response.status === 202) {
-                                                    console.log (`Mensaje enviado al: ${req.body.phone}`)
-                                                }
-                                            });
-                                    }
-
-                                    catch (err) {
-                                        console.error(err);
-                                    }
-                                }
-
-                                else {
-                                    console.log('No envía SMS dando que esta entre las 0 y 15 horas en UTC 0')
-                                }
-                            })
-                            .log(
-                                // console.log
-                            )
-                            .error(
-                                // console.log
-                            )
-                            .debug(
-                                // console.log
-                            )
-
-                            return res.send({success: true, message: `Bienvenido a Ziembra. Enviaremos a tu celular información relevante del producto ${findProduct.name.toUpperCase()}. Aplican T&C.`});
-                        }
-                        catch (e) {
-                            console.log(e);
-                        }
-                    })()                    
-                })                
-            }
-
-            catch (e) {
-
-                if (e.name === 'SequelizeUniqueConstraintError') {
-                    console.log('That user already exists.')
-                }
-
-                console.log('Something went wrong with adding a user.');
-            }
-        }
-        else {
-
-            (async () => {
-                try {
-                    var findProduct = await Product.findOne({ where: { id: resp.productId } });
-
-                    return res.send({success: false, message: `${req.body.name} Ya existe un registro a este número celular con el producto ${findProduct.name}, envíanos un correo a info@ziembra.co para cambiar el producto de interés o incluir productos adicionales.`});
-                }
-                catch (e) {
-                    console.log(e);
-                }
-            })()            
-        }
-    })
+    process.exit(0)
 });
 
 var port = process.env.PORT || 8080;
